6e79aefd0e8a4b90789ccd337a91a9f6f3fcbc79
Antonio Vázquez Blanco <antoniovazquezblanco@gmail.com>
2025-10-07 16:25:44 +0200

    DtbDataTypeLoadTask: Add a DTB header structure.

--- src/main/java/devicetreeblob/DtbDataTypeLoadTask.java
+++ src/main/java/devicetreeblob/DtbDataTypeLoadTask.java
@@ -16,9 +16,11 @@
 package devicetreeblob;
 
 import ghidra.program.model.address.Address;
+import ghidra.program.model.data.DataType;
 import ghidra.program.model.data.DataUtilities;
 import ghidra.program.model.data.DataUtilities.ClearDataMode;
 import ghidra.program.model.data.StructureDataType;
+import ghidra.program.model.data.UnsignedIntegerDataType;
 import ghidra.program.model.listing.Program;
 import ghidra.program.model.symbol.SourceType;
 import ghidra.program.model.symbol.SymbolTable;
@@ -58,8 +60,8 @@ public class DtbDataTypeLoadTask extends Task {
 		// Create data types in the listing view...
 		monitor.setMessage("Creating DTB data type...");
 
-		// Create a structure data type for the DTB
-		StructureDataType dtbStruct = new StructureDataType("dtb_t", mDtbLength);
+		// Create a structure data type for the DTB header
+		StructureDataType dtbStruct = createDtbStructure(mDtbLength);
 
 		// Apply the data type to the DTB location in the program
 		transactionId = mProgram.startTransaction("DTB data type assignment");
@@ -72,5 +74,33 @@ public class DtbDataTypeLoadTask extends Task {
 			Msg.error(getClass(), "Could not assign DTB data type", e);
 		}
 		mProgram.endTransaction(transactionId, ok);
+	}
+
+	private StructureDataType createDtbStructure(int len) {
+		// Create a structure data type for the DTB
+		StructureDataType dtbStruct = new StructureDataType("dtb_t", len);
+
+		StructureDataType dtbHeaderStruct = createDtbHeaderStructure();
+		dtbStruct.insertAtOffset(0, dtbHeaderStruct, dtbHeaderStruct.getLength(), "header", "DTB header");
+		return dtbStruct;
+	}
+
+	private StructureDataType createDtbHeaderStructure() {
+		// Get base types...
+		DataType uintType = UnsignedIntegerDataType.dataType;
+
+		// Create the structure
+		StructureDataType dtbHeaderStruct = new StructureDataType("dtb_header_t", 0);
+		dtbHeaderStruct.add(uintType, 4, "magic", "DTB magic number (0xd00dfeed)");
+		dtbHeaderStruct.add(uintType, 4, "total_size", "Total size of the DTB");
+		dtbHeaderStruct.add(uintType, 4, "off_dt_struct", "Offset to structure block");
+		dtbHeaderStruct.add(uintType, 4, "off_dt_strings", "Offset to strings block");
+		dtbHeaderStruct.add(uintType, 4, "off_mem_rsvmap", "Offset to memory reservation block");
+		dtbHeaderStruct.add(uintType, 4, "version", "Format version");
+		dtbHeaderStruct.add(uintType, 4, "last_comp_version", "Last compatible version");
+		dtbHeaderStruct.add(uintType, 4, "boot_cpuid_phys", "Physical boot CPU ID");
+		dtbHeaderStruct.add(uintType, 4, "size_dt_strings", "Size of strings block");
+		dtbHeaderStruct.add(uintType, 4, "size_dt_struct", "Size of structure block");
+		return dtbHeaderStruct;
 	}
 }
